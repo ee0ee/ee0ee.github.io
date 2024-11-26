7de462327264c345a5b111d9130763ef3914de45
Sam Chen <153130161+ee0ee@users.noreply.github.com>
2024-11-25 20:26:55 -0800

    Returns to index.html

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def is_it_raining_in_seattle():
-    rainheader = "Is it raining in Seattle? \n"
     with urllib.request.urlopen('https://depts.washington.edu/ledlab/teaching/is-it-raining-in-seattle/') as response:
         is_it_raining_in_seattle = response.read().decode()
 
     if is_it_raining_in_seattle == "true":
-        return rainheader + "<h1>Yes</h1>"
+        rainCheck = "<h1>Yes</h1>"
     else:
-        return rainheader + "<h1>No</h1>"
+        rainCheck = "<h1>No</h1>"
+    return render_template("index.html")
